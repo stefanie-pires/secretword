bda420ef18660dbeeb2ff9515825e6becdf16953
Stefanie Pires <stefanie.gs.sg@gmail.com>
2024-04-24 10:27:28 -0300

    first commit

---
+++ src/data/words.jsx
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Marcha", "Pneu", "Antena"],
+    fruta: ["Banana", "Morango", "Kiwi", "Uva", "Laranja"],
+    corpo: ["Olhos", "Perna", "Dedos", "Boca", "Nariz", "Barriga"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React"],
+    alimento: ["Arroz", "Batata", "Carne", "Leite", "Ovo"],
+  };
